d31608e5ff7ff7f153c6f9f88c7760fd1bbf3e88
Ken Giroux <kgiroux@synacor.com>
2015-12-04 07:01:01 -0500

    Completed Day 4 Challenge

--- src/main/java/net/kenmgj/advent/challenges/Day04Challenge.java
+++ src/main/java/net/kenmgj/advent/challenges/Day04Challenge.java
@@ -1,5 +1,8 @@
 package net.kenmgj.advent.challenges;
 
+import java.security.MessageDigest;
+
+import org.apache.commons.codec.binary.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,12 +12,44 @@ public class Day04Challenge extends AbstractChallenge {
 
 	@Override
 	protected int executeChallenge1(String line) {
-		return 0;
+		return hashStartsWith(line, "00000");
 	}
 
 	@Override
 	protected int executeChallenge2(String line) {
-		return 0;
+		return hashStartsWith(line, "000000");
 	}
 
+	private int hashStartsWith(String line, String startsWith) {
+		int i = 0;
+		boolean matchFound = false;
+
+		while (!matchFound) {
+
+			String adventCoin = line + i;
+			logger.debug("Possible AdventCoin: " + adventCoin);
+
+			MessageDigest md5;
+			String md5HashString = "";
+
+			try {
+				 md5 = MessageDigest.getInstance("MD5");
+				 byte[] md5Bytes = md5.digest(adventCoin.getBytes("UTF-8"));
+				 char[] md5Hash = Hex.encodeHex(md5Bytes);
+				 md5HashString = new String(md5Hash);
+				 logger.debug("Hash String: " + md5HashString);
+			} catch (Exception e) {
+			}
+
+			if (md5HashString.startsWith(startsWith)) {
+				logger.debug("Found Match: " + md5HashString);
+				matchFound = true;
+				break;
+			}
+
+			i++;
+		}
+
+		return i;
+	}
 }
