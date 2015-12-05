f0cf3cc1b7e012940b83fc2fa9879ef62ff5af3b
Ken Giroux <kgiroux@synacor.com>
2015-12-05 01:14:18 -0500

    Added placeholder for day 6

---
+++ src/main/java/net/kenmgj/advent/challenges/Day06Challenge.java
@@ -0,0 +1,15 @@
+package net.kenmgj.advent.challenges;
+
+public class Day06Challenge extends AbstractChallenge {
+
+	@Override
+	protected int executeChallenge1(String line) {
+		return 0;
+	}
+
+	@Override
+	protected int executeChallenge2(String line) {
+		return 0;
+	}
+
+}
